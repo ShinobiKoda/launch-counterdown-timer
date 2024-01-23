d92d64e15f19f6ee6db7201f0c8dd89a7a8fa83d
Praise <sirp2804@gmail.com>
2024-01-23 13:38:36 +0100

    fixed design breaking

--- assets/script.js
+++ assets/script.js
@@ -1,41 +1,42 @@
-const flipCard = document.querySelector(".flip-card");
-
-// Calculate last moment of the current month
+// Calculate the end of the current month
 const now = new Date();
-const currentYear = now.getFullYear();
-const currentMonth = now.getMonth();
-const lastDayOfMonth = new Date(currentYear, currentMonth + 1, 0);
-const countToDate = new Date(lastDayOfMonth.setHours(23, 59, 59, 999));
+const countToDate = new Date(
+  now.getFullYear(),
+  now.getMonth() + 1,
+  0,
+  23,
+  59,
+  59,
+  999
+);
 
-
-
+let previousTimeBetweenDates;
 
 setInterval(() => {
   const currentDate = new Date();
   const timeBetweenDates = Math.ceil((countToDate - currentDate) / 1000);
-
   flipAllCards(timeBetweenDates);
+
+  previousTimeBetweenDates = timeBetweenDates;
 }, 250);
 
-const flipAllCards = (time) => {
-  const days = Math.floor(time / 86400);
-  const hrs = Math.floor(time / 3600) % 24;
-  const min = Math.floor(time / 60) % 60;
-  const sec = Math.floor(time % 60);
+function flipAllCards(time) {
+  const seconds = time % 60;
+  const minutes = Math.floor(time / 60) % 60;
+  const hours = Math.floor(time / 3600) % 24;
+  const days = Math.floor(time / (3600 * 24));
 
-  flip(document.querySelector("[data-day-tens]"), Math.floor(days / 10));
-  flip(document.querySelector("[data-day-ones]"), days % 10);
-  flip(document.querySelector("[data-hour-tens]"), Math.floor(hrs / 10));
-  flip(document.querySelector("[data-hour-ones]"), hrs % 10);
-  flip(document.querySelector("[data-minutes-tens]"), Math.floor(min / 10));
-  flip(document.querySelector("[data-minutes-ones]"), min % 10);
-  flip(document.querySelector("[data-seconds-tens]"), Math.floor(sec / 10));
-  flip(document.querySelector("[data-seconds-ones]"), sec % 10);
-};
+  flip(document.querySelector("[data-days-tens]"), Math.floor(days / 10));
+  flip(document.querySelector("[data-days-ones]"), days % 10);
+  flip(document.querySelector("[data-hours-tens]"), Math.floor(hours / 10));
+  flip(document.querySelector("[data-hours-ones]"), hours % 10);
+  flip(document.querySelector("[data-minutes-tens]"), Math.floor(minutes / 10));
+  flip(document.querySelector("[data-minutes-ones]"), minutes % 10);
+  flip(document.querySelector("[data-seconds-tens]"), Math.floor(seconds / 10));
+  flip(document.querySelector("[data-seconds-ones]"), seconds % 10);
+}
 
-
-
-const flip = (flipCard, newNumber) => {
+function flip(flipCard, newNumber) {
   const topHalf = flipCard.querySelector(".top");
   const startNumber = parseInt(topHalf.textContent);
   if (newNumber === startNumber) return;
@@ -45,9 +46,8 @@ const flip = (flipCard, newNumber) => {
   topFlip.classList.add("top-flip");
   const bottomFlip = document.createElement("div");
   bottomFlip.classList.add("bottom-flip");
-  const initial = parseInt(topHalf.textContent);
 
-  topHalf.textContent = startNumber;
+  topFlip.textContent = startNumber;
   bottomHalf.textContent = startNumber;
   topFlip.textContent = startNumber;
   bottomFlip.textContent = newNumber;
@@ -64,8 +64,4 @@ const flip = (flipCard, newNumber) => {
   });
 
   flipCard.append(topFlip, bottomFlip);
-};
-
-flip(flipCard);
-
-
+}
